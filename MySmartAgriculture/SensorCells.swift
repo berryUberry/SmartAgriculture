be4b32bee246fd4994fb523c559fb34e220d1033
berryberry <berryberryry@gmail.com>
2016-05-23 16:44:06 +0800

    change-2

---
+++ MySmartAgriculture/SensorCells.swift
@@ -0,0 +1,21 @@
+//
+//  SensorCells.swift
+//  MySmartAgriculture
+//
+//  Created by 王凯 on 16/5/23.
+//  Copyright © 2016年 joyyog. All rights reserved.
+//
+
+import UIKit
+
+class SensorCells:UITableViewCell{
+
+
+    @IBOutlet weak var sensorImage: UIImageView!
+    
+    @IBOutlet weak var sensorName: UILabel!
+
+    @IBOutlet weak var sensorTem: UILabel!
+    @IBOutlet weak var sensorHnm: UILabel!
+
+}
---
+++ MySmartAgriculture/SensorListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SensorListViewController.swift
+//  MySmartAgriculture
+//
+//  Created by 王凯 on 16/5/23.
+//  Copyright © 2016年 joyyog. All rights reserved.
+//
+
+import UIKit
+
+class SensorListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
